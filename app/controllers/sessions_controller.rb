d5fc9655718f647967972749aa3e567a3c5aa477
Carson Reinke <carson@reinke.co>
2016-12-28 13:57:24 -0500

    Infinite redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create()
     self.current_user = request.env['omniauth.auth'][:uid]
-    redirect_to('/admin')
+    redirect_to('/admin/rails')
   end
 end
