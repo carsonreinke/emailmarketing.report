d0fa3c654bf6932f30c7d00a7f7d84d997e35510
Carson Reinke <carson@reinke.co>
2017-01-02 10:15:05 -0500

    Remove old message queue on failure

--- lib/emails/process.rb
+++ lib/emails/process.rb
@@ -14,12 +14,6 @@ module Emails
 
       site = Site.find_by!({:email_address => mail_message.to})
       site.emails.create!({:message => message})
-    rescue ActiveRecord::RecordNotFound
-      raise $!
-    rescue
-      #Re-queue other errors
-      self.class.message_queue.send(message)
-      raise $!
     end
   end
 end
