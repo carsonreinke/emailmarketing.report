44b2bcada2cd99507fbdffd1087b656afe364371
Carson Reinke <carson@reinke.co>
2017-01-10 14:58:16 -0500

    Updating to correct selected default

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,42 +1,46 @@
+#
+# Copied from Rails
+# https://github.com/rails/rails/blob/master/railties/lib/rails/mailers_controller.rb
+#
 class EmailsController < ApplicationController
   def show()
     @email = Email.find(params[:id]).mail_message()
 
     if params[:part]
-      part_type = Mime::Type.lookup(params[:part])
+      @part_type = Mime::Type.lookup(params[:part])
 
-      if part = find_part(part_type)
-        response.content_type = part_type
+      if part = find_part(@part_type)
+        response.content_type = @part_type
         render plain: part.respond_to?(:decoded) ? part.decoded : part
       else
-        raise AbstractController::ActionNotFound, "Email part '#{part_type}' not found in #{@preview.name}##{@email_action}"
+        raise AbstractController::ActionNotFound, "Email part '#{@part_type}' not found"
       end
     else
       @part = find_preferred_part(request.format, Mime[:html], Mime[:text])
+      @part_type = Mime::Type.lookup(@part.mime_type || request.format)
       render layout: false, formats: %w[html]
     end
   end
 
 
 private
-
-    def find_preferred_part(*formats) # :doc:
-      formats.each do |format|
-        if part = @email.find_first_mime_type(format)
-          return part
-        end
-      end
-
-      if formats.any? { |f| @email.mime_type == f }
-        @email
-      end
-    end
-
-    def find_part(format) # :doc:
+  def find_preferred_part(*formats) # :doc:
+    formats.each do |format|
       if part = @email.find_first_mime_type(format)
-        part
-      elsif @email.mime_type == format
-        @email
+        return part
       end
     end
+
+    if formats.any? { |f| @email.mime_type == f }
+      @email
+    end
+  end
+
+  def find_part(format) # :doc:
+    if part = @email.find_first_mime_type(format)
+      part
+    elsif @email.mime_type == format
+      @email
+    end
+  end
 end
--- test/controllers/emails_controller_test.rb
+++ test/controllers/emails_controller_test.rb
@@ -1,12 +1,12 @@
 require 'test_helper'
 
-class EmailControllerTest < ActionDispatch::IntegrationTest
+class EmailsControllerTest < ActionDispatch::IntegrationTest
   def setup()
     @site = Site.create!({:name => 'Test', :url => 'http://example.com', :email_address => 'test@example.com'})
   end
 
-  test "preview" do
+  test "show" do
     email = @site.emails.create!({:message => Mail::Message.new().encoded()})
-    get email_preview_url(email.id)
+    get email_url(email)
   end
 end
