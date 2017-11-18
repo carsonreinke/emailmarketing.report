a186cf5372d3a0dd2a15ae9eb953d47b3a49c9b6
Carson Reinke <carson@reinke.co>
2017-11-18 11:10:51 -0500

    Don't use this model anymore

--- app/models/chart.rb
+++
@@ -1,6 +0,0 @@
-class Chart < ApplicationRecord
-  store :configuration, {:coder => JSON}
-
-  validates :key, {:presence => true, :uniqueness => {:case_sensitive => true}}
-  validates :configuration, {:presence => true}
-end
